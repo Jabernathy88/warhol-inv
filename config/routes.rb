b6cdf0f71d2ef434a7d333a9089b2df0e8188ab8
Jabernathy88 <j.e.abernathy@gmail.com>
2018-03-11 16:16:11 -0400

    Created controller for Users. Ready to build some seeds.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users do
+      resources :galleries do
+        resources :artworks
+      end
+    end
+  end
+
 end
