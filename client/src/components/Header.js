00368620bc175c868cb1b187b2141721665a15c5
Jabernathy88 <j.e.abernathy@gmail.com>
2018-05-28 21:41:14 -0400

    Small change.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,9 @@ class Header extends Component {
   render() {
     return (
       <header className="cyan lighten-4">
+        {/* logo image here */}
         <h1>Art Banana</h1>
         <h3><span className="blue-grey-text">Inventory manager for art dealers and museum curators</span></h3>
-        <div className="bg-header"></div>
       </header>
     )
   }
